21eb2c8c30935f060e0d3165e4b005a698735665
Marina <mosunovamarina20@gmail>
2026-06-05 16:33:16 +0300

    создали класc Book

---
+++ library/Book.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace library
+{
+    public class Book
+    {
+        public string Title;
+        public string Autor;
+        public int Year;
+        public bool IsAvailable;
+
+        public Book()
+        {
+            Title = "Неизвестная книга";
+            Autor = "Неизвестный автор";
+            Year = 0;
+            IsAvailable = true;
+        }
+
+        public Book(string title, string autor, int year, bool isAvailable)
+        {
+            Title = title;
+            Autor = autor;
+            Year = year;
+            IsAvailable = isAvailable;
+        }
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Название: {Title}");
+            Console.WriteLine($"Автор: {Autor}");
+            Console.WriteLine($"Год издания: {Year}");
+            Console.WriteLine($"Статус книги: {IsAvailable}");
+            if(IsAvailable)
+            {
+                Console.WriteLine("Доступна");
+            }
+            else
+            {
+                Console.WriteLine("Не доступна");
+            }
+
+        }
+        
+    }
+
+}
